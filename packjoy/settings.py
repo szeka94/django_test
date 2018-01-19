a9b6d9f7f40039da4e1baacfe08ebaee1e976f96
szeka94 <szeka1994@gmail.com>
2018-01-19 18:32:48 +0200

    pip stuff added

--- packjoy/settings.py
+++ packjoy/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -92,7 +91,7 @@ WSGI_APPLICATION = 'packjoy.wsgi.application'
 
 
 # Database
-# httpsasdas://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
