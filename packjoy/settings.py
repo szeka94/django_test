af69bc244baab3c2e8a67f687c98ef7a3d842cf9
szeka94 <szeka1994@gmail.com>
2018-01-19 17:31:25 +0200

    test

--- packjoy/settings.py
+++ packjoy/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'packjoy.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# httpsasdas://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
