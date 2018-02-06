5099e9ca3f23dc0605a111a6ab95fc6e75494150
szeka94 <szekelyattilaweb@gmail.com>
2018-02-06 23:45:00 +0200

    js tax section fixed

--- packjoy/settings.py
+++ packjoy/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_MODE', False)
 
-ALLOWED_HOSTS = ['ejoy.pythonanywhere.com',
+ALLOWED_HOSTS = ['www.packjoy.store',
                  'localhost',
                  'api.mailgun.net',
                  'packjoy.store'
