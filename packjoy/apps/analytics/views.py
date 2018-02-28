496fe494712b14c52f69da5cb68e22b79db645a3
szeka94 <szeka1994@gmail.com>
2018-02-28 22:15:17 +0200

    checkout big fixed

--- packjoy/apps/analytics/views.py
+++ packjoy/apps/analytics/views.py
@@ -16,23 +16,24 @@ class EmailAnalytics(View):
 		@TODO: Move this thing inside a job, 
 		shouldn't just hang in here
 		'''
-		body = request.body.decode('utf-8')
-		data = json.loads(body)
-		print(data)
-		if not self.verify(token=data['token'], timestamp=data['timestamp'],
-						   signature=data['signature']):
-			# In this case we want mailgun to retry the request
-			# Any status code can be returned except 406
-			# Returning 406 well cause mailgun to stop trying
-			return HttpRequest(status=400)
+		# body = request.body.decode('utf-8')
+		# data = json.loads(body)
+		# print(data)
+		# if not self.verify(token=data['token'], timestamp=data['timestamp'],
+		# 				   signature=data['signature']):
+		# 	# In this case we want mailgun to retry the request
+		# 	# Any status code can be returned except 406
+		# 	# Returning 406 well cause mailgun to stop trying
+		# 	return HttpRequest(status=400)
 
-		# If the signature is verified we should store the url
-		address = ', '.join([data[fields] for fields in ('country, region, city')])
-		EmailRecord(event=data['event'], recipient=data['recipient'],
-					domain=data['domain'], ip=data['ip'],
-					address=address, mailing_list=data['mailing-list'],
-					user_agent=data['user-agent'])
-		return HttpRequest(status=200)
+		# # If the signature is verified we should store the url
+		# address = ', '.join([data[fields] for fields in ('country, region, city')])
+		# EmailRecord(event=data['event'], recipient=data['recipient'],
+		# 			domain=data['domain'], ip=data['ip'],
+		# 			address=address, mailing_list=data['mailing-list'],
+		# 			user_agent=data['user-agent'])
+		# return HttpRequest(status=200)
+		pass
 
 	def verify(token, timestamp, signature):
 		hmac_digest = hmac.new(key=settings.MAILGUN_ACCESS_KEY,
